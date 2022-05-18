ea18ff6445cd517d3f62bf2ac237c1b1650d8b69
sanghyo <sanghyo@sanghyoui-MacBookPro.local>
2022-05-18 15:52:49 +0900

    lecture2 practice

--- [Stanford] Applications for iOS using SwiftUI/Lecture2/Memorize/Memorize/ContentView.swift
+++ [Stanford] Applications for iOS using SwiftUI/Lecture2/Memorize/Memorize/ContentView.swift
@@ -8,45 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var emojiCount = 4
+    var emojis = ["🚗", "🚌", "🏎", "🚲", "🚍", "🚀", "🛥", "🛶", "✈️", "🚡", "🛵", "🚟", "🛸"]
     
     var body: some View {
         VStack{
-        HStack{
-            CardView()
-            CardView()
-            CardView()
-            CardView()
-            Button{
-                print("clicked")
-            } label:{
-                Text("click")
+            ScrollView{
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]){
+                    ForEach(emojis[0..<emojiCount], id: \.self){emoji in
+                        CardView(emoji: emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
+
             }
-        }
-        .padding(.horizontal)
-        Spacer()
-            HStack{
-                CardView()
-                CardView()
-                CardView()
-                CardView()
-                Button{
-                    print("clicked")
-                } label:{
-                    Text("click")
-                }
             }
             .padding(.horizontal)
-
+            .foregroundColor(.red)
+            Spacer()
+            HStack{
+                minusButton
+                Spacer()
+                plusButton
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
+            }
     }
-}
+    
+    
+    var minusButton: some View{
+        Button(action:{
+            if emojiCount > 1{
+                emojiCount -= 1
+            }
+        }){
+            Image(systemName: "minus.circle")
+            
+        }
+    }
+    
+    var plusButton: some View{
+        Button(action:{
+            if emojiCount < emojis.count{
+                emojiCount += 1
+            }
+        }){
+            Image(systemName: "plus.circle")
+            
+        }
+    }
 }
 
 struct CardView: View{
+    var emoji: String
+    @State private var isFaceUp = true
     var body: some View{
+        let shape = RoundedRectangle(cornerRadius: 20)
         ZStack{
-        RoundedRectangle(cornerRadius: 20)
-            .stroke(lineWidth: 3)
-        Text("hello world")
+            if isFaceUp{
+                shape.fill(.white)
+                shape.strokeBorder(lineWidth: 3)
+                Text(emoji).font(.largeTitle)
+            }else{
+                shape.fill()
+            }
+            
+        }
+        .onTapGesture{
+            self.isFaceUp.toggle()
         }
     }
 }
